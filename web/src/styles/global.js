f965ca6ff890caae133de0e8f61dce4d092a66cf
Micaelli Medeiros <m.amedeiros@hotmail.com>
2020-03-23 23:07:31 -0300

    global styles

--- web/src/styles/global.js
+++ web/src/styles/global.js
@@ -1,6 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 
 import 'react-toastify/dist/ReactToastify.css';
+import 'react-perfect-scrollbar/dist/css/styles.css';
 
 export default createGlobalStyle`
   @import url('https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap');
@@ -19,6 +20,7 @@ export default createGlobalStyle`
   }
   body{
     -webkit-font-smoothing: antialiased;
+    background: linear-gradient(-90deg, #7159c1, #ab59c1);
   }
   body, input, button {
     font: 14px 'Roboto', sans-serif;
