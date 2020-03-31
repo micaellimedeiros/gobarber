acd5ffcf5333b7bc88a924f0c66c3a7540a6182f
Micaelli Medeiros <m.amedeiros@hotmail.com>
2020-03-30 21:31:10 -0300

    added styles to button and input

--- mobile/src/components/Button/index.js
+++ mobile/src/components/Button/index.js
@@ -1,8 +1,27 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
-import { View } from 'react-native';
+import { ActivityIndicator } from 'react-native';
+import PropTypes from 'prop-types';
 
-// import { Container } from './styles';
+import { Container, Text } from './styles';
 
-export default function Button() {
-  return <View />;
+export default function Button({ children, loading, ...rest }) {
+  return (
+    <Container {...rest}>
+      {loading ? (
+        <ActivityIndicator size="small" color="#FFF" />
+      ) : (
+        <Text> {children}</Text>
+      )}
+    </Container>
+  );
 }
+
+Button.propTypes = {
+  children: PropTypes.string.isRequired,
+  loading: PropTypes.bool,
+};
+
+Button.defaultProps = {
+  loading: false,
+};
