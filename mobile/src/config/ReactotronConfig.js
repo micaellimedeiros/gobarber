3fb6867570f84670ca0aaa70e3a8a698c06c7c7c
Micaelli Medeiros <m.amedeiros@hotmail.com>
2020-04-02 12:35:28 -0300

    added: reactotron config with redux

--- mobile/src/config/ReactotronConfig.js
+++ mobile/src/config/ReactotronConfig.js
@@ -1,9 +1,11 @@
 import Reactotron from 'reactotron-react-native';
 import { reactotronRedux } from 'reactotron-redux';
 import reactotronReduxSaga from 'reactotron-redux-saga';
+import AsyncStorage from '@react-native-community/async-storage';
 
 if (__DEV__) {
-  const tron = Reactotron.configure({ host: '192.168.0.14' })
+  const tron = Reactotron.setAsyncStorageHandler(AsyncStorage)
+    .configure({ host: '192.168.0.14' })
     .useReactNative()
     .use(reactotronRedux())
     .use(reactotronReduxSaga())
