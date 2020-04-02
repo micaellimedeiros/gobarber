10da790d43dbed60c93ad8c1232fe6eac1eca18f
Micaelli Medeiros <m.amedeiros@hotmail.com>
2020-04-01 21:56:31 -0300

    added: configs of modules redux and saga

---
+++ mobile/src/store/modules/user/sagas.js
@@ -0,0 +1,33 @@
+/* eslint-disable prefer-object-spread */
+import { all, takeLatest, call, put } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
+
+import api from '~/services/api';
+
+import { updateProfileSuccess, updateProfileFailure } from './actions';
+
+export function* updateProfile({ payload }) {
+  try {
+    // eslint-disable-next-line camelcase
+    const { name, email, avatar_id, ...rest } = payload.data;
+
+    const profile = Object.assign(
+      { name, email, avatar_id },
+
+      rest.oldPassword ? rest : {}
+    );
+
+    const response = yield call(api.put, 'users', profile);
+
+    toast.success('Perfil atualizado com sucesso');
+
+    yield put(updateProfileSuccess(response.data));
+  } catch (err) {
+    toast.error(
+      'Erro ao atualizar perfil, confira seus dados e tente novamente'
+    );
+    yield put(updateProfileFailure());
+  }
+}
+
+export default all([takeLatest('@user/UPDATE_PROFILE_REQUEST', updateProfile)]);
