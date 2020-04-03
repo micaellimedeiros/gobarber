a608a5c3db8ebb664441e7cbe8b8a9cf1a195f5d
Micaelli Medeiros <m.amedeiros@hotmail.com>
2020-04-03 14:04:52 -0300

    feat: button index and styles

--- mobile/src/components/Button/index.js
+++ mobile/src/components/Button/index.js
@@ -1,17 +1,15 @@
-/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import { ActivityIndicator } from 'react-native';
 import PropTypes from 'prop-types';
-
 import { Container, Text } from './styles';
 
 export default function Button({ children, loading, ...rest }) {
   return (
     <Container {...rest}>
       {loading ? (
-        <ActivityIndicator size="small" color="#FFF" />
+        <ActivityIndicator size="small" color="#fff" />
       ) : (
-        <Text> {children}</Text>
+        <Text>{children}</Text>
       )}
     </Container>
   );
--- mobile/src/components/Button/styles.js
+++ mobile/src/components/Button/styles.js
@@ -5,10 +5,10 @@ export const Container = styled(RectButton)`
   height: 46px;
   background: #3b9eff;
   border-radius: 4px;
-
   align-items: center;
   justify-content: center;
 `;
+
 export const Text = styled.Text`
   color: #fff;
   font-weight: bold;
