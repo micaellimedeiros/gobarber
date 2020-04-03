b0f11a33166ba2e6bd6a5980bb83f0b32dc1e4c8
Micaelli Medeiros <m.amedeiros@hotmail.com>
2020-04-03 11:05:36 -0300

    added: index file

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -4,19 +4,17 @@ import { Provider } from 'react-redux';
 import { StatusBar } from 'react-native';
 
 import './config/ReactotronConfig';
-import { store, persistor } from './store';
-import Routes from './routes';
-// import { Container } from './styles';
 
-export default function App() {
+import { store, persistor } from './store';
+import App from './App';
+
+export default function Index() {
   return (
-    <>
-      <Provider store={store}>
-        <PersistGate persistor={persistor}>
-          <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-          <Routes />
-        </PersistGate>
-      </Provider>
-    </>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+        <App />
+      </PersistGate>
+    </Provider>
   );
 }
