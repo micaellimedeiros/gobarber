91e00e1aa1c30b410afe73a1c3e854799549c92d
Micaelli Medeiros <m.amedeiros@hotmail.com>
2020-04-03 14:09:59 -0300

    feat: new rules to yellow warn

--- mobile/index.js
+++ mobile/index.js
@@ -2,8 +2,9 @@
  * @format
  */
 
-import { AppRegistry } from 'react-native';
+import { AppRegistry, YellowBox } from 'react-native';
 import App from './src';
 import { name as appName } from './app.json';
 
+YellowBox.ignoreWarnings(['Calling `getNode()`']);
 AppRegistry.registerComponent(appName, () => App);
